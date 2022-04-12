cf70c68ad7d75a7cf1f996b3077d1d36c399ebe8
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-04-12 23:12:07 +0300

    added:search component

--- src/pages/MyApplication/index.tsx
+++ src/pages/MyApplication/index.tsx
@@ -1,8 +1,28 @@
 
+import Search from './components/Search';
+import Table from './components/Table';
+import { Card, Button, IconArrowLeft24 } from '@dhis2/ui'
 import styles from './MyApplication.module.css';
+import { Link } from 'react-router-dom';
+
 
 function MyApplication() {
-    return (<div>My applicaction</div>);
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <Link to="/">
+                    <Button icon={<IconArrowLeft24 />} name="Primary button" value="default">Back</Button>
+                </Link>
+            </div>
+            <div style={{ padding: "0 20%" }}>
+                <h2>My Applications</h2>
+                <Card style={{ padding: "10px" }}>
+                    <Search />
+                    <Table />
+                </Card>
+            </div>
+        </div>);
 }
 
 export default MyApplication;
