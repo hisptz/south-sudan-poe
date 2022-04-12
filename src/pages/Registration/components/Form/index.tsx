ae2d920a19e08e64ff4022fcb2df1a83cfa4e35f
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-04-12 17:49:39 +0300

    added:registration page

---
+++ src/pages/Registration/components/Form/index.tsx
@@ -0,0 +1,145 @@
+import { Input, FieldGroup, Checkbox, TextArea } from '@dhis2/ui'
+import styles from './Form.module.css';
+
+function Form() {
+    const checkForms: any[] = [
+        'Sudden onset of fever', 'Unexplained Bleeding', 'Muscle Pain', 'Headache', 'loss of appetite',
+        'Vomitting', 'Diarrhea', 'Lethargy', 'Stomach Paign', 'Breathing difficulties', 'Hiccup', 'Cough',
+        'Sore throat', 'Sudden loss of smell', 'Sudden loss of test', 'None of above', 'Other'
+    ];
+
+    const declaration = `I declare that the Information provided is complete and correct to the best of my knowledge.
+    I understand that any false information filled could lead to denial or
+    prosecution in accordance to law.`;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}>
+                    <label>Names</label>
+                </div>
+                <div className={styles["content-container"]}>
+                    <div>
+                        <label htmlFor="firstname">First name</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Middle name</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Last name</label>
+                        <Input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}>
+                    <label>Personal Details</label>
+                </div>
+                <div className={styles["content-container"]}>
+                    <div>
+                        <label htmlFor="firstname">Date of birth</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Age</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Sex</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Nationality</label>
+                        <Input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}>
+                    <label>Contact Informations</label>
+                </div>
+                <div className={styles["content-container"]}>
+                    <div>
+                        <label htmlFor="firstname">Date of birth</label>
+                        <Input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}>
+                    <label>Trips Informantion</label>
+                </div>
+                <div className={styles["content-container"]}>
+                    <div>
+                        <label htmlFor="firstname">Passport number</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Mode of transport</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Point of entry</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Arrival date</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Flight number</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Physical address while in South Sudan</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Phone number while ins South Sudan</label>
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="firstname">Email while in South Sudan</label>
+                        <Input type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}>
+                    <label>Health Informantion</label>
+                </div>
+                <div className={styles["content-container"]}>
+                    <label htmlFor="">Signs and symptoms</label>
+                    <div style={{ padding: "0 0 0 16px" }}>
+                        <FieldGroup>
+                            {checkForms.map(x => <Checkbox label={x} value={x} />)}
+                        </FieldGroup>
+                    </div>
+                    <label htmlFor="">Other Signs and symptoms</label>
+                    <TextArea
+                        name="textAreaName"
+                    />
+                </div>
+            </div>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}>
+                    <label>Declaration of filled Informantion</label>
+                </div>
+                <div className={styles["content-container"]}></div>
+            </div>
+            <div className={styles["form-group"]}>
+                <div className={styles["label-container"]}></div>
+                <div className={styles["content-container"]}>
+                    <div className={styles["checkbox-container"]}>
+                        <Checkbox label={declaration} />
+                    </div>
+                </div>
+            </div>
+            <button>Cancel</button><button>Save</button>
+        </div>
+    );
+}
+
+export default Form;
--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,3 +1,23 @@
-export  default function Registration() {
-    return (<div>Registration</div>);
+import styles from './Registration.module.css'
+import { Card, Button, IconArrowLeft24 } from '@dhis2/ui'
+import Form from './components/Form';
+import { Link } from 'react-router-dom';
+function Registration() {
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <Link to="/">
+                    <Button icon={<IconArrowLeft24 />} name="Primary button" value="default">Back</Button>
+                </Link>
+            </div>
+            <div style={{ padding: "0 20%" }}>
+                <h2>Registration</h2>
+                <Card style={{ padding: "10px" }}>
+                    <Form />
+                </Card>
+            </div>
+        </div>);
 }
+
+export default Registration;
