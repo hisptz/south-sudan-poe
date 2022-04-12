45c337c912c5dfe4f35fa40ad4f1be27e43b0638
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-04-12 22:33:19 +0300

    added:notfound page

--- src/App-routing.tsx
+++ src/App-routing.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import MyApplication from "./pages/MyApplication";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 import Registration from "./pages/Registration";
 import Layout from "./shared/components/Layout";
@@ -10,11 +12,13 @@ export default function MainRouter() {
         <Router>
             <Routes>
                 <Route element={<Layout />} path="/" >
-                    <Route index element={<Home />}/>
-                    <Route element={<Home />} path="/home"/>
+                    <Route index element={<Home />} />
+                    <Route element={<Home />} path="/home" />
                     <Route element={<Registration />} path="/registration" />
                     <Route element={<Profile />} path="/profile/:id" />
+                    <Route element={<MyApplication />} path="/my-application" />
                 </Route>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     )
---
+++ src/pages/MyApplication/index.tsx
@@ -0,0 +1,8 @@
+
+import styles from './MyApplication.module.css';
+
+function MyApplication() {
+    return (<div>My applicaction</div>);
+}
+
+export default MyApplication;
