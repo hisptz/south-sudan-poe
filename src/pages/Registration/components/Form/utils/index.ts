5496738d5110bb1009882d40f42088c563616228
Gift Nnko <nnkogift@hotmail.com>
2022-04-15 00:45:26 +0300

    refactor(Form): Refactor
    - Refactored form operations to a hook

---
+++ src/pages/Registration/components/Form/utils/index.ts
@@ -0,0 +1,19 @@
+import {get} from "lodash";
+
+
+export function sanitizeFields(fields: { [key: string]: any }) {
+    const sanitizedFields = {...fields};
+    const orgUnit = get(fields, "orgUnit");
+    delete sanitizedFields["orgUnit"];
+    const dataValues = Object.keys(sanitizedFields).map(key => {
+        return {
+            dataElement: key,
+            value: sanitizedFields[key]
+        };
+    });
+
+    return {
+        orgUnit,
+        dataValues
+    }
+}
