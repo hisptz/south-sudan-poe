4501b9a5c184556c400cbf2394c44bf4f8f80425
Gift Nnko <nnkogift@hotmail.com>
2022-04-15 04:24:06 +0300

    fix(Styling): Fixes & Improvements
    - Implemented responsiveness to my applications

--- src/pages/MyApplication/components/Search/index.tsx
+++ src/pages/MyApplication/components/Search/index.tsx
@@ -1,10 +1,10 @@
-import { Button, IconArrowLeft24 } from '@dhis2/ui';
+import {Button, InputField} from '@dhis2/ui';
 import styles from './Search.module.css'
 
 function Search() {
     return (
         <div className={styles.container}>
-            <input /> <Button name="Primary button" primary value="default">Search</Button>
+            <InputField/> <Button name="Primary button" primary value="default">Search</Button>
         </div>
     )
 }
--- src/pages/MyApplication/index.tsx
+++ src/pages/MyApplication/index.tsx
@@ -1,31 +1,33 @@
 import Search from "./components/Search";
 import Table from "./components/Table";
-import { Card} from "@dhis2/ui";
+import {Card} from "@dhis2/ui";
 import styles from "./MyApplication.module.css";
 import Back from "../../shared/components/Back";
 
 const MyApplication = () => {
 
-  return (
-    <div className={styles.container}>
-      <Back />
-      <div className="content-body">
-        <h2>My Applications</h2>
-        <Card style={{ padding: "10px" }}>
-          <div className={styles["inner-container"]}>
-            <div className={styles.search}>
-              <Search />
+    return (
+        <div className={styles.container}>
+            <div style={{paddingLeft: 16, paddingTop: 16}}>
+                <Back/>
             </div>
-          </div>
-          <div className={styles["inner-container"]}>
-            <div className={styles.table}>
-              <Table />
+            <div className="content-body">
+                <h2>My Applications</h2>
+                <Card style={{padding: "10px"}}>
+                    <div className={styles["inner-container"]}>
+                        <div className={styles.search}>
+                            <Search/>
+                        </div>
+                    </div>
+                    <div className={styles["inner-container"]}>
+                        <div className={styles.table}>
+                            <Table/>
+                        </div>
+                    </div>
+                </Card>
             </div>
-          </div>
-        </Card>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default MyApplication;
