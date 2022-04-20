aeafa416b4fdc8f163679f3db3b2d4f77c7c4fa1
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-04-20 14:44:47 +0300

    style: override DHIS2 ui primary color

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,29 +9,33 @@ const Home = () => {
     return (
         <div className={styles.container}>
             <h1>WELCOME TO SOUTH SUDAN</h1>
-            <p style={{textAlign: "justify"}}>We are glad that you have chosen to travel to South Sudan. For your safety
+            <p style={{textAlign: "center"}}>We are glad that you have chosen to travel to South Sudan. For your safety
                 and the well-being of those around you, please be informed <br/>
                 that it is mandatory to complete the Public Health Surveillance Form online within 24 hours before
                 arrival. You can start by searching <br/>
                 your passport on the field above and complete the travellers form. </p>
             <div className={styles["cards-container"]}>
                 <div className={styles.card}>
-                    <Link to="registration">
+                    <Link to="registration" style={{textDecoration:"none",fontSize:18}}>
                         <Card>
                             <img src={registerLogo} alt="register avatar"/>
                         </Card>
-                        <label style={{color: '#001660'}}
+                       <div style={{"padding":10}}>
+                       <label style={{color: "var(--primary)"}}
                                htmlFor="register label">Register</label>
+                       </div>
                     </Link>
                 </div>
                 <div className={styles.card}>
 
-                    <Link to="my-application">
+                    <Link to="my-application" style={{textDecoration:"none",fontSize:18}}>
                         <Card>
                             <img src={myApplicationLogo} alt="my application avatar"/>
                         </Card>
-                        <label style={{color: '#001660'}}
+                        <div style={{"padding":10}}>
+                        <label style={{color: "var(--primary)"}}
                                htmlFor="register label">My-application</label>
+                        </div>
                     </Link>
                 </div>
             </div>
