616300be65148e36d1a4780d64a99f079042d7e4
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-04-21 13:17:30 +0300

    style: added state in language dropdown

--- src/shared/components/Dropdown/index.tsx
+++ src/shared/components/Dropdown/index.tsx
@@ -1,30 +1,29 @@
-import { Layer, Popper, FlyoutMenu, MenuItem, IconChevronDown16 } from '@dhis2/ui'
-import { useRef, useState } from 'react'
-
+import { Layer, Popper, FlyoutMenu, MenuItem, IconChevronDown16 } from "@dhis2/ui";
+import { useRef, useState } from "react";
 
 function Dropdown(args: any) {
-
-    const ref = useRef<HTMLButtonElement | any>()
-    const [open, setOpen] = useState(false)
+    const ref = useRef<HTMLButtonElement | any>();
+    const [open, setOpen] = useState(false);
+    const [language, setLanguage] = useState("en");
     const toggle = () => setOpen(!open);
 
     return (
         <>
             <button ref={ref} onClick={toggle}>
-                Choose language &nbsp;&nbsp; <IconChevronDown16 />
+                {language == "en" ? "English" : "Arabic"} &nbsp;&nbsp; <IconChevronDown16 />
             </button>
             {open && (
                 <Layer onClick={toggle}>
                     <Popper reference={ref} placement="bottom-start">
                         <FlyoutMenu {...args}>
-                            <MenuItem label="English" />
-                            <MenuItem label="Swahili" />
+                            <MenuItem label="English" onClick={() => setLanguage("en")} />
+                            <MenuItem label="Arabic" onClick={() => setLanguage("ar")} />
                         </FlyoutMenu>
                     </Popper>
                 </Layer>
             )}
         </>
-    )
+    );
 }
 
 export default Dropdown;
