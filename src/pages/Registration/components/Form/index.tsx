f6d599318882a6d4055cdf60d695eb78704a7c7b
Danford Kija <kijadanford@gmail.com>
2022-04-29 12:53:38 +0300

    feat(accordion):add accordion into forms               - add accordion into register form               - add accordion into edit form

--- src/pages/Registration/components/Form/index.tsx
+++ src/pages/Registration/components/Form/index.tsx
@@ -8,9 +8,22 @@ import useFormControl from "./hooks/form";
 import Loader from "../../../../shared/components/Loader";
 import Error from "../../../../shared/components/Error";
 import i18n from '@dhis2/d2-i18n';
+import {IconChevronDown24	} from "@dhis2/ui";
+import { Accordion, AccordionDetails, AccordionSummary,Typography } from "@mui/material";
 
 
 const Form = () => {
+    const [expanded, setExpanded] = React.useState(false);
+
+      
+
+
+    const handleChange = (panel:any) => (event:any, isExpanded:any) => {
+       console.log(panel, isExpanded,event);
+    //    setExpanded(isExpanded ? panel : false);
+
+      };
+
     const {loading, error, sections, dataElements, onSubmit, form, saving} = useFormControl();
 
     if (loading) {
@@ -25,20 +38,38 @@ const Form = () => {
 
     if (!sections) return <div>No form metadata</div>;
 
+
+
     return (
         <FormProvider {...form}>
             <form className={styles.container} onSubmit={form.handleSubmit(onSubmit)}>
                 {sections &&
                     sections?.map(
-                        (x: any, key: any) => (
+                        (sectionPosition: any, key: any) => (
+                            <Accordion 
+                            elevation={0}
+
+                          
+                            defaultExpanded={sectionPosition.displayFormName === "Names"?true:false} square={true} disableGutters={true} TransitionProps={{ unmountOnExit: true }} onChange={handleChange('panel1')}>
+                            <AccordionSummary
+                              expandIcon={<IconChevronDown24 />}
+                              aria-controls="panel1a-content"
+                              id="panel1a-header"
+                            >
+                              <Typography>{sectionPosition.displayFormName}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
                             <FormBuilder
                                 key={"form-builder" + key}
-                                title={x.displayFormName}
-                                controls={x.dataElements}
+                                title={""}
+                                controls={sectionPosition.dataElements}
                                 stageDataElements={
                                     dataElements
                                 }
                             />
+                            </AccordionDetails>
+                          </Accordion>
+                          
                         )
                     )}
 
