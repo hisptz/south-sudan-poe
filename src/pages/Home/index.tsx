db86632feb52ab96fc6c2ffe1a0485279380ded4
Gift <nnkogift@hotmail.com>
2022-05-05 16:17:01 +0300

    fix(home): Fixes
    - Fixed issues with card styling
     - Fixed issues with intro text not covering all  screen for mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <h1>{i18n.t("WELCOME TO SOUTH SUDAN")}</h1>
-      <p style={{ textAlign: "center", width: "50%" }}>
+      <p className={styles["intro"]}>
         {i18n.t(
           "We are glad that you have chosen to travel to South Sudan. For your safety and the well-being of those around you, please be informed that it is mandatory to complete the Public Health Surveillance Form online within 24 hours before arrival. You can start by searching your passport on the field above and complete the travellers form."
         )}
