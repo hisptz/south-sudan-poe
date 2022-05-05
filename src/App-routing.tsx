869f5e036105b2d113cba2be20dcbce45687027a
Gift <nnkogift@hotmail.com>
2022-05-05 18:46:00 +0300

    :bug: fix(routing): Improvements
    - Added lazy imports for routes

--- src/App-routing.tsx
+++ src/App-routing.tsx
@@ -1,27 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import MyApplication from "./pages/MyApplication";
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import NotFound from "./pages/NotFound";
-import Profile from "./pages/Profile";
-import Registration from "./pages/Registration";
-import { HomeSearch } from "./pages/Registration/components/Search/Search";
+import {HomeSearch} from "./pages/Registration/components/Search/Search";
 import Layout from "./shared/components/Layout";
 
+const MyApplication = React.lazy(() => import("./pages/MyApplication"));
+const Home = React.lazy(() => import("./pages/Home"));
+const Profile = React.lazy(() => import("./pages/Profile"));
+const Registration = React.lazy(() => import("./pages/Registration"));
+
 export default function MainOutlet() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route element={<Layout />} path="/">
-          <Route index element={<Home />} />
-          <Route element={<Home />} path="/home" />
-          <Route element={<Registration />} path="/registration" />
-          <Route element={<Registration />} path="/registration/:id" />
-          <Route element={<Profile />} path="/profile/:id" />
-          <Route element={<MyApplication />} path="/my-application" />
-          <Route element={<HomeSearch />} path="/search" />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename={process.env.PUBLIC_URL}>
+            <Routes>
+                <Route element={<Layout/>} path="/">
+                    <Route index element={<Home/>}/>
+                    <Route element={<Home/>} path="/home"/>
+                    <Route element={<Registration/>} path="/registration"/>
+                    <Route element={<Registration/>} path="/registration/:id"/>
+                    <Route element={<Profile/>} path="/profile/:id"/>
+                    <Route element={<MyApplication/>} path="/my-application"/>
+                    <Route element={<HomeSearch/>} path="/search"/>
+                </Route>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Router>
+    );
 }
