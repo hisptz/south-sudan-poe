178875b357901e47a55978e1251e257ca4c09782
Gift <nnkogift@hotmail.com>
2022-05-17 17:42:53 +0300

    :sparkles: feat(app toolbar): Implementation
    - Added South Sudan flag to header
     - Improved styling and placement of language selector

--- src/shared/components/Layout/index.tsx
+++ src/shared/components/Layout/index.tsx
@@ -1,39 +1,46 @@
-import { Outlet, useLocation } from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import Toolbar from "./components/Toolbar";
 import classes from "./Layout.module.css";
-import { ErrorBoundary } from "react-error-boundary";
+import {ErrorBoundary} from "react-error-boundary";
 import Error from "../Error";
-import React, { Suspense } from "react";
+import React, {Suspense} from "react";
 import Loader from "../Loader";
 import Back from "../Back";
+import LanguageSelector from "../Dropdown";
 
 function Layout() {
-  const location = useLocation();
-  const hideBackButton = location.pathname === "/";
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Toolbar />
-      <div className={classes["content"]}>
-        {!hideBackButton && (
-          <div style={{ paddingLeft: 16, paddingTop: 16 }}>
-            <Back />
-          </div>
-        )}
-        <ErrorBoundary resetKeys={[location]} FallbackComponent={Error}>
-          <Suspense fallback={<Loader small />}>
-            <Outlet />
-          </Suspense>
-        </ErrorBoundary>
-      </div>
-    </div>
-  );
+    const location = useLocation();
+    const hideBackButton = location.pathname === "/";
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <Toolbar/>
+            <div className={classes["content"]}>
+                <div
+                    style={hideBackButton ? {justifyContent: "flex-end"} : undefined}
+                    className={classes["action-row"]}>
+                    {!hideBackButton && (
+                        <Back/>
+                    )}
+                    <div
+                        className={classes["language-selector"]}>
+                        <LanguageSelector/>
+                    </div>
+                </div>
+                <ErrorBoundary resetKeys={[location]} FallbackComponent={Error}>
+                    <Suspense fallback={<Loader small/>}>
+                        <Outlet/>
+                    </Suspense>
+                </ErrorBoundary>
+            </div>
+        </div>
+    );
 }
 
 export default Layout;
